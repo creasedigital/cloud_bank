8c4a182bd117dc12f03bd6ad9fa4907da3d96c58
creasedigital <ojorechris@gmail.com>
2022-05-25 13:22:41 +0100

    illustration links updated

--- src/data/index.js
+++ src/data/index.js
@@ -1,41 +1,49 @@
 export const cards = [
 	{
 		id: 1,
+		icon: "assets/illustrations/One Month.svg",
 		title: "One Month Loan",
 		desc: "Access funds for personal upkeep for 30 days only",
 	},
 	{
 		id: 2,
+		icon: "assets/illustrations/Working Capital Overdraft.svg",
 		title: "Working Capital Overdraft",
 		desc: "Access continuous funds to grow your business stocks and inventory",
 	},
 	{
 		id: 3,
+		icon: "assets/illustrations/Operation Overdraft.svg",
 		title: "Operation Overdraft",
 		desc: "Financing for your business operations",
 	},
 	{
 		id: 4,
+		icon: "assets/illustrations/T&T.svg",
 		title: "T and T Spread Loan",
 		desc: " Purchase phones, generators and vehicles on a reasonable payment plan from T and T Deals Market",
 	},
 	{
 		id: 5,
+		icon: "assets/illustrations/Infrastructure.svg",
 		title: "Infrastrusture Loan",
 		desc: "Facility for your business to purchase vehicles, assets, and building upgrades.",
 	},
 	{
 		id: 6,
+		icon: "assets/illustrations/personal.svg",
 		title: "Personal Loan",
 		desc: "Instant funds for your personal expenses.",
 	},
 	{
 		id: 7,
+		icon: "assets/illustrations/Pawn.svg",
 		title: "Pawn Loan",
 		desc: "Get cash value on temporary sale of your items within 72hours.",
 	},
 	{
 		id: 8,
+		icon: "assets/illustrations/Frame 33.svg",
 		title: "FX line",
 		desc: "Access Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
 	},
