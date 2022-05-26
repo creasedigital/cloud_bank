5698c5d31b8a27b351fc89bf66ae787f19dbb0cc
creasedigital <ojorechris@gmail.com>
2022-05-26 21:47:14 +0100

    active class bug fixed

--- src/components/SideNavItem.js
+++ src/components/SideNavItem.js
@@ -3,11 +3,11 @@ import { menuLinks } from "../data";
 
 const SideNavItem = () => {
 	return (
-		<div className="text-navGreen">
+		<div className="text-navGreen ">
 			{menuLinks.map((menuLink) => (
 				<NavLink to={menuLink.path}>
 					{" "}
-					<button className="flex p-2 " key={menuLink.id}>
+					<button className="flex p-2 w-full" key={menuLink.id}>
 						<img alt="icon" className="pr-2" src={menuLink.icon} />
 						<span className="">{menuLink.title}</span>
 					</button>
